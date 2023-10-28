3c7c432960ae1e73a3315506f76912d5e62a6947
Barun Karmakar <kumarbarun137@gmail.com>
2023-10-28 12:03:02 +0530

    initial upload

---
+++ backend/index.js
@@ -0,0 +1,8 @@
+//import 
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+
+app.use(bodyParser.urlencoded({extended:true}));
