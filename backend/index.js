5c2feb0cd47eb06947c2c89f5397f9bb4014b6d3
dipankarwebsadroit <75478236+dipankarwebsadroit@users.noreply.github.com>
2023-11-13 12:13:45 +0530

    13-11-23

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 app.use(cors());
 // coonect to mongodb
 connectToMongoDB();
-const publicFolderPath = path.join(__dirname, 'images');
+const publicFolderPath = path.join(__dirname, 'uploads');
 app.use(express.static(publicFolderPath));
 app.use(express.json({ limit: "50mb" }));
 app.use("/realEstate/api", route);
